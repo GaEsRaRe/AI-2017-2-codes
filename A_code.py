874aca2babecfdbb15a5f7fca8c1fac2531cd8b0
DESKTOP-6O5F3M6\Gabriel PC <g_e_ramirez@outlook.com>
2017-08-23 22:46:37 -0500

    A* project Basic Code Structure, simple graph implementation

---
+++ A_code.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 20:31:59 2017
+
+@author: Gabriel PC
+"""
+
+#A* blind proyect
+
+import numpy as np
+
+
+class node: #Easy to mod node
+    id_number = 0
+    data = ""
+    pass
+
+class graph:
+    data = [] #includes every node
+    mapping = [] #include of paths
+    def add(self,x):
+        self.data.append(x)
+        pass
+    def check(self,a,b): #check if the path is possible
+        testing = False
+        t1,t2 = False, False
+    
+        for i in range(0,np.size(self.data)):
+            if self.data[i] == a:
+                t1 = True
+            if self.data[i] == b:
+                t2 = True
+        if t1 == True and t2 == True:
+            testing = True
+        if a == b:
+            testing = False
+        return testing
+    def knot(self,a,b,size): #add a path between two nodes
+        valid = self.check(a,b)
+        if valid == False:
+            print("Don't exist")
+            pass
+        temp = [a,b,size]
+        self.mapping.append(temp)
+    pass
+
