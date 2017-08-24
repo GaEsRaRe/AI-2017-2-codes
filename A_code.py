bf42c3ddaa889fe22ba1ad29426623f23d8444f1
DESKTOP-6O5F3M6\Gabriel PC <g_e_ramirez@outlook.com>
2017-08-23 23:54:50 -0500

    Not a lot of progress, just pushed to keep it updated

--- A_code.py
+++ A_code.py
@@ -18,13 +18,19 @@ class node: #Easy to mod node
 class graph:
     data = [] #includes every node
     mapping = [] #include of paths
+    entropy = [] # (a,e,lenght) is a basic lineal distance between two points given as data
+    
+    def define_entropy(self,array): #Let us add an entropy mapping
+        self.entropy = array
+        pass
+    
     def add(self,x):
         self.data.append(x)
         pass
+    
     def check(self,a,b): #check if the path is possible
         testing = False
         t1,t2 = False, False
-    
         for i in range(0,np.size(self.data)):
             if self.data[i] == a:
                 t1 = True
@@ -35,6 +41,7 @@ class graph:
         if a == b:
             testing = False
         return testing
+    
     def knot(self,a,b,size): #add a path between two nodes
         valid = self.check(a,b)
         if valid == False:
@@ -42,5 +49,14 @@ class graph:
             pass
         temp = [a,b,size]
         self.mapping.append(temp)
+
+    def pathfinding(self,a,b):
+        entry = []
+        visited = []
+        path_finded = False
+        while path_finded == False:
+            None #temp data
     pass
+
+
 
